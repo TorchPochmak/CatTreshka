4f5c34e380b68472bf184cdbfdc8ad302e066e83
TorchPochmak <exprompar@gmail.com>
2023-07-10 20:23:46 +0300

    Revert "Puzzle...."
    This reverts commit 6096ef121a1a5495ba617567c573c884aa268077.

--- Assets/Scripts/PuzzleScripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleScripts/PuzzleManager.cs
@@ -4,52 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-namespace CatTreshka
+public class PuzzleManager : MonoBehaviour
 {
-    public class PuzzleManager : MonoBehaviour
+    [SerializeField] private PuzzleRotate[] puzzles;
+
+    public int allTiles = 6;
+    public int solvedTiles = 6;
+    public GameObject lamp;
+
+    private void Start()
     {
-        [SerializeField] private PuzzleRotate[] puzzles;
+        puzzles = FindObjectsOfType<PuzzleRotate>();
+    }
+    public void GenerateField()
+    {
+        // paste your code here
+    }
 
-        public int allTiles = 6;
-        public int solvedTiles = 6;
-        public GameObject lamp;
+    public Vector3 SetZ(Vector3 vector, float z)
+    {
+        vector.z = z;
+        return vector;
+    }
 
-        private void Start()
+
+
+    public void CheckPuzzle()
+    {
+        solvedTiles = 0;
+        for (int i = 0; i < puzzles.Length; i++)
         {
-            puzzles = FindObjectsOfType<PuzzleRotate>();
-        }
-        public void GenerateField()
-        {
-            // paste your code here
-        }
-
-        public Vector3 SetZ(Vector3 vector, float z)
-        {
-            vector.z = z;
-            return vector;
-        }
-
-
-
-        public void CheckPuzzle()
-        {
-            solvedTiles = 0;
-            for (int i = 0; i < puzzles.Length; i++)
+            if (puzzles[i].isPlaced)
             {
-                if (puzzles[i].isPlaced)
-                {
-                    solvedTiles++;
-                }
+                solvedTiles++;
             }
-
-            if (solvedTiles == allTiles)
-            {
-                // send action to other scripts
-                lamp.transform.position = SetZ(lamp.transform.position, -2);
-                Debug.Log("solved");
-            }
-
         }
+
+        if (solvedTiles == allTiles)
+        {
+            // send action to other scripts
+            lamp.transform.position = SetZ(lamp.transform.position, -2);
+            Debug.Log("solved");
+        }
+
     }
 }
